a2e35c04fc4b880ddf0b9ea1901801022b294d8d
damon <damon.default@gmail.com>
2013-10-21 17:11:48 +1100

    Fixed up basePath in servlet to be relative to context root if relative. Otherwise absolute.

--- js-class-loader/src/main/java/jsclassloader/servlet/JsClassLoaderServlet.java
+++ js-class-loader/src/main/java/jsclassloader/servlet/JsClassLoaderServlet.java
@@ -1,8 +1,10 @@
 package jsclassloader.servlet;
 
+import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,27 +17,19 @@ import jsclassloader.Config;
 @SuppressWarnings("serial")
 public class JsClassLoaderServlet extends HttpServlet {
 
-	private Config jsClassLoaderConfig;
 	private String propFileName = "js-class-loader.properties";
-	
+	private Config config;
 	@Override
-	public void init(final ServletConfig config) throws ServletException {
-		super.init(config);
-		try {
-			jsClassLoaderConfig = new Config(
-				this.getClass().getClassLoader().getResourceAsStream(propFileName));
-		}
-		catch(IOException ioe) {
-			throw new ServletException(
-					"JSClassLoader servlet can't load properties file '" + propFileName + 
-					"' from classpath\n" + 
-					"message: " + ioe.getMessage());
-		}
+	public void init(ServletConfig servletConfig) throws ServletException {
+		super.init(servletConfig);
+		this.config = getConfig(servletConfig.getServletContext());
+		
 	}
+	
 	@Override
 	protected void doGet(final HttpServletRequest request, final HttpServletResponse response) {
 		try {
-			Bundler bundler = new Bundler(jsClassLoaderConfig);
+			Bundler bundler = new Bundler(config);
 			response.setContentType("text/javascript");
 			
 			response.setContentLength(bundler.getContentLength());
@@ -43,6 +37,29 @@ public class JsClassLoaderServlet extends HttpServlet {
 		}
 		catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private Config getConfig(ServletContext context) throws ServletException {
+		try {
+			Config config = new Config(
+				this.getClass().getClassLoader().getResourceAsStream(propFileName));
+			
+			String basePath = config.getProperty(Config.PROP_BASE_FOLDER);
+			
+			//Make sure all paths are relative to the context root:
+			if (!new File(basePath).isAbsolute()) {
+				String relativeBase = context.getRealPath("/" + basePath);
+				config.setProperty(Config.PROP_BASE_FOLDER, relativeBase);
+			}
+			
+			return config;
+		}
+		catch(IOException ioe) {
+			throw new ServletException(
+					"JSClassLoader servlet can't load properties file '" + propFileName + 
+					"' from classpath\n" + 
+					"message: " + ioe.getMessage());
 		}
 	}
 }
